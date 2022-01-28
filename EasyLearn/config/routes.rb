539612003787dd11a5f607e76e16f73a037c2f88
javs1 <javierverduzco13@gmail.com>
2022-01-28 17:39:11 -0600

    Login view done

--- EasyLearn/config/routes.rb
+++ EasyLearn/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'login/index'
+  get "/login", to: "login#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
