fedcf3d88452e59a7b9f373df6d640754ff41214
javs1 <javierverduzco13@gmail.com>
2022-02-03 18:13:30 -0600

    Initialize DB structure and foreign keys
    Co-authored-by: Joshsam31 <samano82324@gmail.com>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    
    Created tables
    Migrations done

--- EasyLearn/db/schema.rb
+++ EasyLearn/db/schema.rb
@@ -10,9 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_03_224648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.string "day", null: false
+    t.string "hour", null: false
+    t.string "link", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "preferences", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.string "email", default: "", null: false
+    t.string "password", null: false
+    t.string "avatar_profile"
+    t.bigint "preference_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.date "date", null: false
+    t.string "link", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
