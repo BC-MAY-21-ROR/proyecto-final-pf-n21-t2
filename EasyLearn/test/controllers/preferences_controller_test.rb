20caafe1fa1729a352425aae988b72cd144618c8
MauricioNC <mauricio_navarro0@ucol.mx>
2022-02-11 15:24:38 -0600

    Preferences view finished
    - It was created the preferences.css file to change some styles.
    - By now, the content HTML is static, but it will be replaced with
      dynamic content once we start with the functionality.
    
    Co-authors:
    Co-authored-by: javs1 <javierverduzco13@gmail.com>
    Co-authored-by: Joshsam31 <samano82324@gmail.com>

--- EasyLearn/test/controllers/preferences_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PreferencesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get preferences_index_url
-    assert_response :success
-  end
-end
