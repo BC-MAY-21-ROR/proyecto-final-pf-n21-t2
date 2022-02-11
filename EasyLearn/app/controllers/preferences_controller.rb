36538017bf7414e7f387e0b6d9744cc169719183
MauricioNC <mauricio_navarro0@ucol.mx>
2022-02-10 18:01:28 -0600

    Preferences view finished
    - Created the preferences view.
    - The view has static content, but in the future,  them will be replaced
      with dynamic content.

--- EasyLearn/app/controllers/preferences_controller.rb
+++ EasyLearn/app/controllers/preferences_controller.rb
@@ -1,6 +1,13 @@
 class PreferencesController < ApplicationController
-  def preferences
-    
-  end
+
   
+  def preferences 
+    if params[:data]
+      respond_to do |format|
+        format.html do
+          "sdfsdfs"
+        end
+      end
+    end
+  end
 end
