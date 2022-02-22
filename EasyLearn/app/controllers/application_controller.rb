3c7ecf95af17340199b0791a159fbe4d3ad76309
Joshsam31 <samano82324@gmail.com>
2022-02-22 13:43:05 -0600

    Changes done

--- EasyLearn/app/controllers/application_controller.rb
+++ EasyLearn/app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
   helper_method :current_user
   private 
-  helper_method :current_user, :logged_in?
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
