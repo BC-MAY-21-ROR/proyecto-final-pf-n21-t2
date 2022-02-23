8e264c2a3ceddb781158df3897d2bce013c29b84
Joshsam31 <samano82324@gmail.com>
2022-02-23 01:10:09 -0600

    changes done

--- EasyLearn/db/migrate/20220219211841_change_password_to_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangePasswordToUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :password
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
-  end
-end
--- EasyLearn/db/migrate/20220221205606_change_preference_to_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangePreferenceToUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :preference_id
-  end
-end
