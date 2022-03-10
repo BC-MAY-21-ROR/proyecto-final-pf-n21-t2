dfab965b469fbe1dd8cbd628ae69f0ec93406875
Joshsam31 <samano82324@gmail.com>
2022-03-09 20:25:11 -0600

    settings functionality done

--- EasyLearn/app/controllers/settings_controller.rb
+++
@@ -1,5 +0,0 @@
-class SettingsController < ApplicationController
-  before_action :require_login
-  def index
-  end
-end
--- EasyLearn/app/controllers/users_controller.rb
+++ EasyLearn/app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :require_login
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user2 = User.find_by(email:params[:email])
@@ -16,16 +18,25 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-    if @current_user.update(:name => params[:name], :email => params [:email], :description => params[:description], :password => params[:password])
-      
+  def editUser
+    @user = @current_user
   end
 
-  def user_params
+  def edit
+    @user = @current_user
+    if @user.update(update_params)
+      redirect_to root_path
+    end
+  end
+
+  private
+  def user_params 
     params.require(:user).permit(:name, :email, :password)
   end
-  def update_params
-    params.require(:user).permit(:name, :email, :description, :password)
-  end 
- 
+  def update_params 
+    params.require(:user).permit(:name, :email, :description ,:password)
+  end
+
+  
+
 end
