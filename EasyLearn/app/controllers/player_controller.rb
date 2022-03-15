01197e80035b3c7eafd1addd9d6ba846593c51d6
MauricioNC <mauricio_navarro0@ucol.mx>
2022-03-14 18:50:24 -0600

    Player functionality
    To show the video player, we send the video title to the controller and
    redirect to player view.
    
    - Player controller:
     - The controller catch the param from home view and make a request to
       get the respective video.
     - Once obtained the video, the controller iterate response and get the
       author (the user that uploaded the video) and get all the other videos
       to show them as a videos related.
     - Also, we get all the events and get the author of that event to show
       them.

--- EasyLearn/app/controllers/player_controller.rb
+++ EasyLearn/app/controllers/player_controller.rb
@@ -3,12 +3,17 @@ class PlayerController < ApplicationController
   def index
     @videoPlayer = Video.where(title: params[:v_title])
 
-    @videoPlayer.each do |owner|
-      @userID = owner.user_id
-      @catID = owner.category_id
+    @videoPlayer.each do |auth|
+      @author = User.where(id: auth.user_id)
+      @videoRelated = Video.where.not(id: auth.id)
     end
 
-    @owner = User.where(id: @userID)
-    @videoRelated = Video.where(category_id: @catID)
+    # Getting the events
+    @events = Event.all
+    @eventAuthor = []
+
+    @events.each do |e|
+      @eventAuthor << User.find_by(id: e.user_id)
+    end
   end
 end
